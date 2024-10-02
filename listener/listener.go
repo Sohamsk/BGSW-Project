7a055b889e4d1e47b0f5268b9264be5f7fffeeb3
Soham Kudalkar <sohamskudalkar02@gmail.com>
2024-10-02 21:38:54 +0530

    expression parsing with list

--- listener/listener.go
+++ listener/listener.go
@@ -4,7 +4,9 @@ import (
 	"bosch/parser"
 	"bosch/stack"
 	"bufio"
+	"bytes"
 	"fmt"
+	"strings"
 
 	"github.com/antlr4-go/antlr/v4"
 )
@@ -22,16 +24,16 @@ func NewTreeShapeListener(writer *bufio.Writer) *TreeShapeListener {
 	return l
 }
 
-func (s *TreeShapeListener) VisitTerminal(node antlr.TerminalNode) {
-	// 218 token type is variable name
-//	if node.GetSymbol().GetTokenType() == 218 {
-//		fmt.Println(node.GetText())
-		// figure something out to place the variable name and their types somewhere
-		// i will figure something out to make the it so that the varibale declaration statement state is stored
-		// i am thinking of using a stack to save the state to use later while constructing the tree
-//	}
-	//fmt.Println(node.GetSymbol().String())
-}
+//func (s *TreeShapeListener) VisitTerminal(node antlr.TerminalNode) {
+//	// 218 token type is variable name
+////	if node.GetSymbol().GetTokenType() == 218 {
+////		fmt.Println(node.GetText())
+//		// figure something out to place the variable name and their types somewhere
+//		// i will figure something out to make the it so that the varibale declaration statement state is stored
+//		// i am thinking of using a stack to save the state to use later while constructing the tree
+////	}
+//	//fmt.Println(node.GetSymbol().String())
+//}
 
 func (s *TreeShapeListener) EnterStartRule(ctx *parser.StartRuleContext) {
 	s.writer.WriteString("\"body\": [")
@@ -54,38 +56,41 @@ func (s *TreeShapeListener) EnterVariableSubStmt(ctx *parser.VariableSubStmtCont
 	s.writer.WriteString("},")
 }
 
-func handleLetExpression(nodes []antlr.Tree) {
+func handleLetExpression(nodes []antlr.Tree, w *bufio.Writer, first bool) {
 	if (len(nodes) == 0) {
 		return
 	}
-	fmt.Println("\"Left\": {")
-	outer:
-	for i, node := range(nodes) {
+	if (!first && len(nodes) == 1) {
+		fmt.Print(",")
+	}
+	for _, node := range(nodes) {
 		switch node.(type) {
 		case antlr.TerminalNode:
 			if (node.(antlr.TerminalNode).GetText() != " ") {
-				fmt.Println(node.(antlr.TerminalNode).GetText())
-				fmt.Println("}\"Right\": {")
-				fmt.Println("}")
-				handleLetExpression(nodes[i + 1:])
-				break outer
+				w.WriteString("{\"Operator\": \"" + node.(antlr.TerminalNode).GetText() + "\"},")
 			}
 		case antlr.RuleNode:
 			if (node.(antlr.RuleNode).GetChildCount() == 1) {
-				fmt.Println(node.(antlr.RuleNode).GetText())
+				w.WriteString("{\"Identifier\": \"" + node.(antlr.RuleNode).GetText() + "\"},")
 			} else {
-				handleLetExpression(node.(antlr.RuleNode).GetChildren())
+				handleLetExpression(node.(antlr.RuleNode).GetChildren(), w, false)
 			}
 		}
 	}
-	fmt.Println("}")
 }
 func (s *TreeShapeListener) EnterLetStmt(ctx *parser.LetStmtContext) {
-	//fmt.Println(parser.VisualBasic6ParserParserStaticData.RuleNames[ctx.GetRuleIndex()])
+	fmt.Println(parser.VisualBasic6ParserParserStaticData.RuleNames[ctx.GetRuleIndex()])
 	nodes := ctx.GetChildren()
-	fmt.Println("{")
-	handleLetExpression(nodes)
-	fmt.Println("__________")
+	var buffer bytes.Buffer
+	writer := bufio.NewWriter(&buffer)
+
+	s.writer.WriteString("{\"RuleType\": \"expression\", \"Body\": ")
+	writer.WriteString("[")
+	handleLetExpression(nodes, writer, true)
+	writer.Flush()
+	str := buffer.String()
+	str = strings.TrimRight(str, ",")
+	s.writer.WriteString(str + "]},")
 }
 
 func (s *TreeShapeListener) EnterDoLoopStmt(ctx *parser.DoLoopStmtContext) {
--- test.bas
+++ test.bas
@@ -1,7 +1,7 @@
 Dim I as INTEGER
 Dim A as VARIANT
-I = 5
-A = I + 5
+I = -5
+A = I + (-5)
 S = A + 2 + Func(Y)
 Do While Not I = 0
 	Beep
