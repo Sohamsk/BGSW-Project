715baf3e6632b8d340f25d2699d0208e38dd3438
Soham Kudalkar <sohamskudalkar02@gmail.com>
2024-10-17 11:55:31 +0530

    taking condition and the kind of do loop into account

--- listener/listener.go
+++ listener/listener.go
@@ -42,7 +42,7 @@ func (s *TreeShapeListener) exitContext() {
     s.writer.Flush()
     string := s.buf.String()
     string = strings.Trim(string, ",") + "]"
-    fmt.Println(string)
+   // fmt.Println(string)
     s.buf.Reset()
     s.writer.WriteString(string)
 }
@@ -275,7 +275,27 @@ func (s *TreeShapeListener) EnterECS_ProcedureCall(ctx *parser.ECS_ProcedureCall
 }
 
 func (s *TreeShapeListener) EnterDoLoopStmt(ctx *parser.DoLoopStmtContext) {
+	rules := parser.VisualBasic6ParserParserStaticData.RuleNames
     s.writer.WriteString("{\"RuleType\":\"DoLoopStatement\",")
+    nodes := ctx.GetChildren()
+    for _, node := range nodes {
+        _, ok := node.(antlr.TerminalNode)
+        if (ok) {
+            if (strings.ToLower(node.(antlr.TerminalNode).GetText()) == "while" || strings.ToLower(node.(antlr.TerminalNode).GetText()) == "until") {
+                s.writer.WriteString("\"Kind\":\"" + strings.ToLower(node.(antlr.TerminalNode).GetText()) + "\",")
+            }
+            continue
+        } else if (!ok) {
+            if (rules[node.(antlr.RuleContext).GetRuleIndex()] == "valueStmt") {
+                var buffer bytes.Buffer
+                writer := bufio.NewWriter(&buffer)
+                handleLetExpression(node.(antlr.RuleContext).GetChildren(), writer)
+                writer.Flush()
+                s.writer.WriteString("\"Condition\": [" + strings.Trim(buffer.String(), ",") + "],")
+            }
+        }
+
+    }
     s.writer.WriteString("\"Body\": [")
 }
 func (s *TreeShapeListener) ExitDoLoopStmt(ctx *parser.DoLoopStmtContext) {
