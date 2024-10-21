e2b530d974007ca48555a9aa0bc4c7d0a3dc4c7e
gen1us <coder.sam.1100@gmail.com>
2024-10-21 12:29:50 +0530

    fixed trailing comma, handled condition and body

--- listener/ifthenelse.go
+++ listener/ifthenelse.go
@@ -2,6 +2,9 @@ package listener
 
 import (
 	"bosch/parser"
+	"bufio"
+	"bytes"
+	"strings"
 )
 
 func (s *TreeShapeListener) EnterIfBlockStmt(ctx *parser.IfBlockStmtContext) {
@@ -11,15 +14,18 @@ func (s *TreeShapeListener) EnterIfBlockStmt(ctx *parser.IfBlockStmtContext) {
 		switch node := node.(type) {
 		case *parser.IfConditionStmtContext:
 			s.writer.WriteString("\"IsBlock\":\"True\",")
-			IfCondition := node.GetText()
-			s.writer.WriteString("\"Condition\":\"{" + IfCondition + "}\",")
-			s.writer.WriteString("\"IfBlock\": ")
+			var buffer bytes.Buffer
+			writer := bufio.NewWriter(&buffer)
+			handleLetExpression(node.GetChild(0).GetChildren(), writer)
+			writer.Flush()
+			s.writer.WriteString("\"Condition\": [" + strings.Trim(buffer.String(), ",") + "],")
+			s.writer.WriteString("\"IfBlock\": [")
 		}
 	}
 }
 
 func (s *TreeShapeListener) ExitIfBlockStmt(ctx *parser.IfBlockStmtContext) {
-    s.exitContext()
+	s.exitContext()
 	s.writer.WriteString("},")
 }
 
@@ -29,29 +35,33 @@ func (s *TreeShapeListener) EnterIfElseIfBlockStmt(ctx *parser.IfElseIfBlockStmt
 	for _, node := range nodes {
 		switch node := node.(type) {
 		case *parser.IfConditionStmtContext:
-			IfCondition := node.GetText()
-			s.writer.WriteString("\"Condition\":\"{" + IfCondition + "}\",")
-			s.writer.WriteString("\"ElseIfBlock\": ")
+			var buffer bytes.Buffer
+			writer := bufio.NewWriter(&buffer)
+			handleLetExpression(node.GetChild(0).GetChildren(), writer)
+			writer.Flush()
+			s.writer.WriteString("\"Condition\": [" + strings.Trim(buffer.String(), ",") + "],")
+			s.writer.WriteString("\"ElseIfBlock\": [")
 		}
 	}
 }
 
 func (s *TreeShapeListener) ExitIfElseIfBlockStmt(ctx *parser.IfElseIfBlockStmtContext) {
-    s.exitContext()
+	s.exitContext()
 	s.writer.WriteString("},")
 }
 
 func (s *TreeShapeListener) EnterIfElseBlockStmt(ctx *parser.IfElseBlockStmtContext) {
-	s.writer.WriteString("{\"ElseBlock\": ")
+	s.writer.WriteString("{\"RuleType\":\"ElseBlock\",")
+	s.writer.WriteString("\"Body\": [")
 
 }
 
 func (s *TreeShapeListener) ExitIfElseBlockStmt(ctx *parser.IfElseBlockStmtContext) {
-    s.exitContext()
+	s.exitContext()
 	s.writer.WriteString("}")
 }
 
 func (s *TreeShapeListener) ExitBlockIfThenElseStmt(ctx *parser.BlockIfThenElseContext) {
-    s.exitContext()
+	s.exitContext()
 	s.writer.WriteString("}")
 }
